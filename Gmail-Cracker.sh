92c14237ba424af8bd9e754fd077dbfa9c851df4
ABOL_HACKER <abolfazlm3419.2@gmail.com>
2024-07-17 22:46:19 +0330

    Add files via upload

---
+++ Gmail-Cracker.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Function to update the repository
+update_repository() {
+    echo "Updating repository..."
+    bash update.sh
+    echo "Repository updated successfully!"
+}
+
+# Main script
+clear
+echo
+pkg install pv -y >/dev/null 2>&1
+echo -e "\033[32m\033[1m{───────────────────────────────────────────────────}"
+echo -e "\033[33m\033[1m   Installing All Required Packages! Please Wait..." | pv -qL 10
+apt update                    
+apt upgrade -y 
+pkg install bash
+pkg install php
+pkg install pv -y 
+apt install figlet -y  
+echo -e "\033[31m\033[1m        INSTALLATION COMPLETED \033[32m[\033[36m✓\033[32m]" | pv -qL 12
+echo -e "\033[33m\033[1m]────────────────────────────────────────────["
+termux-setup-storage
+cd $HOME/Hack-Gmail.
+
+# Run login script
+php termux.php
+
